b36aa482136217a67d84529a5bf8fbbaf2f8a2ce
Daniel Moll <dan.moll@icloud.com>
2016-03-29 15:36:33 +0100

    chore: updated to latest version PRC. Updated tests and CSS as per PR.

--- test/index.js
+++ test/index.js
@@ -1,34 +1,57 @@
-
+import 'babel-polyfill';
 import Mythbuster from '../src';
-import React from 'react/addons';
+import React from 'react';
 import chai from 'chai';
-const TestUtils = React.addons.TestUtils;
-chai.should();
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+chai.use(chaiEnzyme()).should();
+
 describe('Mythbuster', () => {
+  const images = [
+    {
+      sources: [
+        {
+          url: '/assets/slide.svg',
+          mime: 'image/svg+xml',
+        },
+      ],
+    },
+  ];
+  const defaultSlide = 0;
   it('renders a React element', () => {
     React.isValidElement(<Mythbuster />).should.equal(true);
   });
 
+  describe('handleInteraction', () => {
+    let rendered = null;
+    let mythbuster = null;
+    beforeEach(() => {
+      rendered = mount(<Mythbuster images={images} defaultSlide={defaultSlide} />);
+      mythbuster = rendered.find('.mythbuster');
+    });
+    it('toggles the active class', () => {
+      mythbuster.simulate('click');
+      mythbuster.should.have.className('mythbuster--active');
+      mythbuster.simulate('click');
+      mythbuster.should.not.have.className('mythbuster--active');
+    });
+  });
+
   describe('Rendering', () => {
-    const renderer = TestUtils.createRenderer();
-    const images = [
-      {
-        sources: [
-          {
-            url: '/assets/slide.svg',
-            mime: 'image/svg+xml',
-          },
-        ],
-      },
-    ];
-    const defaultSlide = 0;
-    it('renders a mythbuster', () => {
-      renderer.render(<Mythbuster items={images} defaultSlide={defaultSlide} />);
-      const render = renderer.getRenderOutput();
-      render.props.className.should.equal('mythbuster mythbuster--slide-0');
-      render.props.children.length.should.equal(2);
-      render.props.children[1].props.children.props.activeSlide.should.equal(0);
-      render.props.children[1].props.children.props.images.length.should.equal(1);
+    let rendered = null;
+    let mythbuster = null;
+    beforeEach(() => {
+      rendered = mount(<Mythbuster images={images} defaultSlide={defaultSlide} />);
+      mythbuster = rendered.find('.mythbuster');
+    });
+
+    it('renders a top level div.mythbuster', () => {
+      mythbuster.should.have.tagName('div');
+      mythbuster.should.have.className('mythbuster');
+    });
+
+    it('renders a className with the active slide', () => {
+      mythbuster.should.have.className('mythbuster--slide-0');
     });
   });
 });
